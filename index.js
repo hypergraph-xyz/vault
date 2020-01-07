4a1b410358bb837ca633cdbe7568c9d741b96fff
Julian Gruber <julian@juliangruber.com>
2020-01-07 13:55:07 +0100

    add GET /health

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const routes = require('./lib/http-routes')
 const { WEBHOOK_SECRET: webhookSecret } = process.env
 
 const handler = async (req, res) => {
-  const { post } = routes(req)
+  const { get, post } = routes(req)
 
-  if (post('/stripe')) {
+  if (get('/health')) {
+    res.end('OK')
+  } else if (post('/stripe')) {
     const body = await promisify(textBody)(req, res)
     const sig = req.headers['stripe-signature']
     const event = stripe.webhooks.constructEvent(body, sig, webhookSecret)
