d6611729bae70d58c4b04b5f2ca9e4c161dd435e
Julian Gruber <julian@juliangruber.com>
2020-01-08 13:40:26 +0100

    add uptime to /health, refactor

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const textBody = require('body')
 const { promisify } = require('util')
 const http = require('./lib/http-handler')
 const routes = require('./lib/http-routes')
+const { json } = require('./lib/http-respond')
 
 const { WEBHOOK_SECRET: webhookSecret } = process.env
 
@@ -12,13 +13,15 @@ const handler = async (req, res) => {
   const { get, post } = routes(req)
 
   if (get('/health')) {
-    res.end('OK')
+    json(res, {
+      uptime: process.uptime()
+    })
   } else if (post('/stripe')) {
     const body = await promisify(textBody)(req, res)
     const sig = req.headers['stripe-signature']
     const event = stripe.webhooks.constructEvent(body, sig, webhookSecret)
     console.log('stripe', event)
-    res.end(JSON.stringify({ received: true }))
+    json(res, { received: true })
   }
 }
 
