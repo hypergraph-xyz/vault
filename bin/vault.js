12c4259206dc1216c22068a098b1c887c261e453
Julian Gruber <julian@juliangruber.com>
2020-01-13 14:55:48 +0100

    move migration script to entrypoint. #15

--- bin/vault.js
+++ bin/vault.js
@@ -4,9 +4,21 @@ process.title = 'vault'
 
 const handler = require('..')
 const http = require('http')
+const { default: migrate } = require('node-pg-migrate')
 
 const { PORT: port = 8080 } = process.env
 
-const server = http.createServer(handler)
-server.listen(port)
-console.log('http://localhost:8080')
+const main = async () => {
+  await migrate({
+    dir: `${__dirname}/../migrations`
+  })
+
+  const server = http.createServer(handler)
+  server.listen(port)
+  console.log('http://localhost:8080')
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
