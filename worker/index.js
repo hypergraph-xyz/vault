f51598b3b9c8d02dbca33412fcf6195cef7aac96
Julian Gruber <julian@juliangruber.com>
2020-01-23 16:50:16 +0100

    worker: add basic replication (#22)

--- worker/index.js
+++ worker/index.js
@@ -1,20 +1,52 @@
 'use strict'
 
 const fetch = require('node-fetch')
-const P2PCommons = require('@p2pcommons/sdk-js')
+const hyperdrive = require('@geut/hyperdrive-promise')
+const ram = require('random-access-memory')
+const hyperswarm = require('hyperswarm')
+const { toBuf, toStr } = require('dat-encoding')
+const { promisify } = require('util')
+const { pipeline } = require('stream')
 
 class Worker {
   constructor ({ vaultUrl }) {
     this.vaultUrl = vaultUrl
-    this.p2p = new P2PCommons()
-    this.ready = this.p2p.ready()
   }
 
   async start () {
-    await this.ready
+    const drives = new Map()
+
+    const swarm = hyperswarm({ queue: { multiplex: true } })
+    swarm.on('error', console.error)
+    swarm.on('connection', (socket, info) => {
+      socket.setMaxListeners(Infinity)
+      for (const topic of info.topics) {
+        const drive = drives.get(toStr(topic))
+        if (drive) {
+          const isInitiator = info.client
+          pipeline(
+            socket,
+            drive.replicate(isInitiator, { timeout: 0 }),
+            socket,
+            err => err && console.error(err)
+          )
+          console.log('replicate', toStr(topic))
+        }
+      }
+    })
+    await promisify(swarm.listen.bind(swarm))()
+
     const res = await fetch(`${this.vaultUrl}/api/modules`)
     const modules = await res.json()
-    console.log({ modules })
+
+    for (const { url } of modules) {
+      const key = toBuf(url)
+      const drive = hyperdrive(ram, key)
+      await promisify(drive.once.bind(drive))('ready')
+      drives.set(toStr(drive.discoveryKey), drive)
+      console.log('join', toStr(drive.discoveryKey))
+      swarm.join(drive.discoveryKey, { announce: true, lookup: true })
+    }
   }
 }
 
