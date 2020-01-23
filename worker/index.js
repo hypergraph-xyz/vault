8f12bb782a64ea1259cdd45d201871266222d1bc
Julian Gruber <julian@juliangruber.com>
2020-01-23 12:39:06 +0100

    worker: add p2p commons sdk

--- worker/index.js
+++ worker/index.js
@@ -1,13 +1,17 @@
 'use strict'
 
 const fetch = require('node-fetch')
+const P2PCommons = require('@p2pcommons/sdk-js')
 
 class Worker {
   constructor ({ vaultUrl }) {
     this.vaultUrl = vaultUrl
+    this.p2p = new P2PCommons()
+    this.ready = this.p2p.ready()
   }
 
   async start () {
+    await this.ready
     const res = await fetch(`${this.vaultUrl}/api/modules`)
     const modules = await res.json()
     console.log({ modules })
