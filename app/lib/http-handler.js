9a25c31585c53013306c9db674aff842f057f39b
Julian Gruber <julian@juliangruber.com>
2020-02-10 16:40:54 +0100

    http-handler: improve status message handling

--- app/lib/http-handler.js
+++ app/lib/http-handler.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const { NODE_ENV: env } = process.env
+const { format } = require('util')
+const { STATUS_CODES } = require('http')
 
 // This library provides some sane http behavior around async functions, some
 // of which is known from popular frameworks like express.
@@ -9,14 +11,28 @@ const { NODE_ENV: env } = process.env
 //     and text
 //   - If no response was sent, a 404 will be used
 //   - Useful request information will be logged
+//   - Stack traces will be printed during development for 5xx errors
 
 module.exports = handler => (req, res) => {
   const start = new Date()
   handler(req, res)
     .catch(err => {
-      console.error(err)
       res.statusCode = err.statusCode || 500
-      const msg = env === 'production' ? 'Internal Server Error' : err.stack
+      if (err.statusCode >= 500) console.error(err)
+      let msg
+      if (res.statusCode < 500) {
+        if (/%s/.test(err.message)) {
+          msg = format(err.message, STATUS_CODES[res.statusCode])
+        } else {
+          msg = err.message
+        }
+      } else {
+        if (env === 'production') {
+          msg = 'Internal Server Error'
+        } else {
+          msg = err.stack
+        }
+      }
       res.end(msg)
     })
     .then(() => {
