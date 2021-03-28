84c3d999bcd88835ae91d292f748ccb040303957
Chris Hartgerink <chris@libscie.org>
2021-03-28 13:40:32 +0200

    Update /discover

--- index.js
+++ index.js
@@ -146,7 +146,11 @@ const handler = async (req, res) => {
         'hypergraph://2ae99d1441b7605a9775b2e1c7835d4ddaaafc9472b0a0fd6e9bf9607dd210b5',
         'hypergraph://d462250eb6d04c1ff2d4aa4b468f74419ab52a682e9f9d2805fde7baf6e46795',
         'hypergraph://f6caa70b49c2dde0f884f739d3096deb998af9d5feecda38f75bd1477c3c3ff3',
-        'hypergraph://051a4e699409bc354157d4820978c6dded0816161881c54d33ff9d0de38903cf+4'
+        'hypergraph://051a4e699409bc354157d4820978c6dded0816161881c54d33ff9d0de38903cf+4',
+        'hypergraph://4faae28c5f61d90d8cd04a545b4814daa719c01b6e03e14dae33f986edf69208+17',
+        'hypergraph://8f694fbdf324cbb83deaecb4a82d13fbff817d8237726cbe0c3f0aa37c13ae11+6',
+        'hypergraph://3999d78fcbdf4544f9f3a9028956c572e012a2181454a79ffe5e560746888e3f+8',
+        'hypergraph://27fbb1cf8413b28d29ace15282cda9080f225f3750e2078c986aff4ff82831be+9'
       ])
       break
     }
